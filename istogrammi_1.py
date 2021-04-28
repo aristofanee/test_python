6a1e8745fdbe9ca55427750e756a399f47792a49
aristofanee <75656110+aristofanee@users.noreply.github.com>
2021-04-28 19:34:48 +0200

    Add files via upload

---
+++ istogrammi_1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 28 19:26:14 2021
+
+@author: davide notaro
+"""
+
+#creatore di istogrammi
+#giuro non so perchè l'ho messo su github
+
+lista_input = list()
+
+lista_input = [2, 4, 6, 1, 2 ,6]
+
+print("\n")
+      
+for x in lista_input:
+    print(x*"*")
+    
